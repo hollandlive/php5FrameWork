71056421ea1248a253dffe8579b79d419a8f848a
hollandlive <dutchnewsru@gmail.com>
2014-12-12 16:01:16 +0100

    Example
    Example Extended

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 require_once 'library/index.class.php';
 $moduleA1 = new Module('This is usual Module');
 $moduleA2 = new Featured_Module('THIS IS UNUSUAL MODULE!');
-$columnA = new Column('This is first column');
-$columnB = new Column('This is 2 column');
+$columnA = new Column();
+$columnA->append($moduleA1);
+$columnA->append($moduleA2);
+$columnA->append(new Module('This is a module A3')); //this way its also possible
+$columnA->render();
 $page = new Page();
 $page->append($columnA);
 $page->render();
