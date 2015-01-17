6f8a1aa607b6aa7ea0f2182007bde6ae6bc8b0ca
hollandlive <dutchnewsru@gmail.com>
2015-01-17 19:47:52 +0100

    Update wp-apm-temp.php

--- wp-apm-temp.php
+++ wp-apm-temp.php
@@ -1,71 +1,65 @@
-<?php 
+<?php
 
-/** Ik kijk over deze twee plagins, APM en WP UF dit weekend nog
- */
+/* * Plugin Name: Advertisement 
+ * 
+ *Plugin URI: http://dutchnews.ru/ 
+ *Author URI: http://bwawwp.com 
+ ** Description: This is a working folder of the plugin. 
+ ** Author: Artem, Rotterdam
+ *
+ ** Version: 1.0.0 
+ ** 
+ ** License: GPLv2 */
+		
+function at_wp_footer($variableA) {
+			$variableA = "I am still working on two plugins: APM and WP UF";
+			echo $variableA;
+	}
+add_action ('wp_footer', 'at_wp_footer');
 
 
 
-class apmAdType {
+function at_load_scripts() {        
+	if (is_admin()) {                
+		wp_enqueue_script(                        
+		'advertisement-plugin-admin',                        
+		plugins_url('js/admin.js', __FILE__ ),                        
+		array('jquery'),                        
+		SCHOOLPRESS_VERSION                
+		);        
+	} else {                
+		wp_enqueue_script(                        
+		'advertisement-plugin-frontend',                        
+		plugins_url('js/frontend.js', __FILE__ ),                        
+		array('jquery'),                        
+		SCHOOLPRESS_VERSION                
+		);        
+	} 
+} 
+add_action('init', 'at_load_scripts');
 
-    protected $key = 'tribe_post_status';
-    protected $type = 'post_status';
 
-    public function __construct() {
-        $type = $this->type;
-        
-        add_action( 'init', array($this, 'test_filters') );
 
-    /**    add_filter('tribe_custom_column'.$type, array($this, 'column_value'), 10, 3 );
-        add_filter('tribe_custom_row'.$type, array($this, 'form_row'), 10, 4 );
-        add_filter('tribe_maybe_active'.$type, array($this, 'maybe_set_active'), 10, 3 );
-        add_action('tribe_after_parse_query', array($this, 'parse_query'), 10, 2 );
-        add_action('tribe_orderby_custom'.$type, array($this, 'orderby'), 10, 2 );
-    }
-add_action( 'wp', 'sp_stub_preheader', 1 );
+function at_load_styles() {        
+	if (is_admin()) {                
+		wp_enqueue_style(                        
+		'advertisement-plugin-admin',                        
+		plugins_url('css/admin.css', __FILE__ ),                        
+		array(),                        
+		ADVERTISEMENT_VERSION,                        
+		'screen'                
+		);        
+	} else {                
+		wp_enqueue_style(                        
+		'advertisement-plugin-frontend',                        
+		plugins_url('css/frontend.css', __FILE__ ),                        
+		array(),                        
+		ADVERTISEMENT_VERSION,                        
+		'screen'                
+		);        
+	} 
+} 
 
-    public function orderby($wp_query, $filter) {
-        add_filter( 'posts_orderby', array($this, 'set_orderby'), 10, 2 );
-    }
-
-    public function set_orderby($orderby, $wp_query) {
-        // run once
-        remove_filter( 'posts_orderby', array($this, 'set_orderby'), 10, 2 );
-        global $wpdb;
-        list($by, $order) = explode(' ', trim($orderby) );
-        $by = "{$wpdb->posts}.post_status";
-        return $by . ' ' . $order;
-    }
-
-    public function parse_query($wp_query, $active) {
-        if ( ! isset($active[$this->key]) ) {
-            return;
-        }
-        $status = $active[$this->key]['value'];
-        $wp_query->set('post_status', $status);
-    }
-
-    public function maybe_set_active($return, $key, $filter) {
-        if ( isset($_POST[$key]) && ! empty($_POST[$key]) ) {
-            return array('value' => $_POST[$key]);
-        }
-        return $return;
-    }
-
-    public function form_row($return, $key, $value, $filter) {
-        $stati = get_post_stati(array('show_in_admin_status_list'=>true), 'objects');
-        $args = array();
-        foreach ( $stati as $k => $object ) {
-            $args[$k] = $object->label;
-        }
-        return tribe_select_field($key, $args, $value['value'], true);
-    }
-
-    public function column_value($value, $column_id, $post_id) {
-        $status = get_post_status($post_id);
-        $status_object = get_post_status_object($status);
-        return ( isset($status_object->label) ) ? $status_object->label : $status;
-    }
-
-}**/
-new apmAdType;
+add_action('init', 'at_load_styles'); 
 ?>
+
